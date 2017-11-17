54818b7ee3410a5ac553410daf15a09afb723ac7
Eun Jae Lee <eunjaelee@hotmail.com>
2017-11-17 13:20:10 +1100

    main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+//
+//  main.c
+//  While loop
+//
+//  Created by Eun Jae Lee on 17/11/17.
+//  Copyright © 2017 Eun Jae Lee. All rights reserved.
+//
+// condition is true run - require initialization, condition and incresement & decrement
+// while (condition) {statement1;
+//                      statemen2; }
+
+
+#include <stdio.h>
+
+int main() {
+
+    int i = 0;
+    
+    while (i<=20) {
+        printf("%d \n",i);
+        i++;
+    }
+    
+    return 0;
+}
